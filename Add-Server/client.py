f64019aaa2ab5c232d98c3bd99480033a90e1f6e
Rishabh Abhani <rishabhabhani@gmail.com>
2021-04-14 16:39:18 +0530

    update

--- Add-Server/client.py
+++ Add-Server/client.py
@@ -1,26 +1,26 @@
 import socket
 
-host = socket.gethostname()
+host = 'raspberrypi.local'
 port = 42069
 
 s = socket.socket()
 s.connect((host, port))
 
 while True:
-  a = input("Enter first number: ")
-  b = input("Enter second number: ")
-  c = f"{a},{b}"
-  print(f"Sending {c} to server...")
+    a = input("Enter first number: ")
+    b = input("Enter second number: ")
+    c = f"{a},{b}"
+    print(f"Sending {c} to server...")
 
-  if a == "-1" or b == "-1":
-    s.sendall("-1".encode('utf-8'))
-    data = s.recv(1024).decode('utf-8')
-    print(data)
-    break
+    if a == "-1" or b == "-1":
+        s.sendall("-1".encode('utf-8'))
+        data = s.recv(1024).decode('utf-8')
+        print(data)
+        break
 
-  else:
-    s.sendall(c.encode('utf-8'))
-    data = s.recv(1024).decode('utf-8')
-    print(int(data))
+    else:
+        s.sendall(c.encode('utf-8'))
+        data = s.recv(1024).decode('utf-8')
+        print(int(data))
 
 s.close()
--- Add-Server/server.py
+++ Add-Server/server.py
@@ -1,25 +1,26 @@
 import socket
 
-host = socket.gethostname()
+host = ''
 port = 42069
 s = socket.socket()
 s.bind((host, port))
 
 s.listen(5)
+print(f'Listening at {host}:{port}')
 
 conn, addr = s.accept()
 print(f"Connection by {addr}")
 
 while True:
-  data = conn.recv(1024).decode('utf-8')
-  if(data == "-1"):
-    print("Breaking connection with the client...")
-    conn.sendall("Breaking connection with the server...".encode('utf-8'))
-    break
-  d = data.split(",")
-  try:
-    data_add = int(d[0]) + int(d[1])
-    conn.sendall(str(data_add).encode('utf-8'))
-  except:
-    break
+    data = conn.recv(1024).decode('utf-8')
+    if(data == "-1"):
+        print("Breaking connection with the client...")
+        conn.sendall("Breaking connection with the server...".encode('utf-8'))
+        break
+    d = data.split(",")
+    try:
+        data_add = int(d[0]) + int(d[1])
+        conn.sendall(str(data_add).encode('utf-8'))
+    except:
+        break
 conn.close()
